83c4c6cb01b95a9e2ea6ed676cfe9f6c1153d791
shreyamane1526 <shreyamane1526@gmail.com>
2025-11-01 19:46:21 +0530

    Time: 3 ms (18.81%), Space: 45.7 MB (5.44%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,22 +1,24 @@
 class Solution {
-    public static void powset(int[] nums,List<List<Integer>> l2,List<Integer> l1,int target,int i){
+    public static void powset(int[] nums, int target,List<List<Integer>> l2,List<Integer> l1,int i){
         if(i==nums.length){
             if(target==0){
-                l2.add(new ArrayList<>(l1));
+                if(!l2.contains(new ArrayList<>(l1))){
+                    l2.add(new ArrayList<>(l1));
+                }
             }
             return;
         }
-        if(target>=nums[i] ){
+        if(target>=nums[i]){
             l1.add(nums[i]);
-            powset(nums,l2,l1,target-nums[i],i);
+            powset(nums,target-nums[i],l2,l1,i);
             l1.remove(l1.size()-1);
         }
-        powset(nums,l2,l1,target,i+1);
+        powset(nums,target,l2,l1,i+1);
     }
     public List<List<Integer>> combinationSum(int[] nums, int target) {
         List<List<Integer>> l2=new ArrayList<>();
         List<Integer> l1=new ArrayList<>();
-        powset(nums,l2,l1,target,0);
+        powset(nums,target,l2,l1,0);
         return l2;
     }
 }
