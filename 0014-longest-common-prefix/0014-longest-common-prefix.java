3109793eb226f2cf2cf6ab4b7a2b2fe0784455b2
shreyamane1526 <shreyamane1526@gmail.com>
2025-11-07 01:29:52 +0530

    Time: 2 ms (16.99%), Space: 43.2 MB (7.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -2,10 +2,6 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         StringBuilder p=new StringBuilder(strs[0]);
         for(int i=1;i<strs.length;i++){
-            // if(p.toString().contains(strs[i])){
-            //     p=new StringBuilder(strs[i]);
-            // }
-            // else{
                 int j=0;
                 for( j=0;j<p.length() && j<strs[i].length();j++){
                     if(strs[i].charAt(j)!=p.charAt(j)){
@@ -13,7 +9,6 @@ class Solution {
                     }
                 }
                 p.delete(j,p.length());
-           // }
             if(p==new StringBuilder("")){
                 return "";
             }
