567621f7c69f46e2157b80ad00e462122011f782
shreyamane1526 <shreyamane1526@gmail.com>
2025-11-07 15:35:05 +0530

    Time: 1 ms (86.88%), Space: 41.8 MB (7.31%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2){return x;}
+        int ans=0,l=0,r=x/2;
+        while(l<=r){
+            long mid=l+(r-l)/2;
+            long sq=mid*mid;
+            if(x== sq){
+                return (int)mid;
+            }
+            else if(x< sq){
+                r=(int)mid-1;
+            }
+            else{
+                ans=(int)mid;
+                l=(int)mid+1;
+            }
+        }
+        return ans;
+    }
+}
