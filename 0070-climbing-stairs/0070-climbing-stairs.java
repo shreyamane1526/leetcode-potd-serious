1d4183dc16dfe93a1c67118573513ad41f4dc4bf
shreyamane1526 <shreyamane1526@gmail.com>
2025-11-08 22:39:24 +0530

    Time: 0 ms (100%), Space: 42.1 MB (6.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,28 @@
 class Solution {
-    public int climbStairs(int n) {
+    public static int fib_memotd(int n,int dp[]){
         if(n<=2){
             return n;
         }
-        int a=1,b=2,ans=0;
-        for(int i=3;i<=n;i++){
-            ans=a+b;
-            a=b;
-            b=ans;
+        if(dp[n]!=-1){
+            return dp[n];
         }
-        return ans;
+        dp[n]=fib_memotd(n-1,dp)+fib_memotd(n-2,dp);
+        return dp[n];
+        //tc O(N)  sc=O(N)
+    }
+    public static int fib_rec(int n){
+        if(n<=2){
+            return n;
+        }
+        return fib_rec(n-1)+fib_rec(n-2);
+        //tc= O(2^n)
+        //sc=O(N)-recursion stack 
+        //got tle 
+    }
+    public int climbStairs(int n) {
+      // return fib_rec(n);
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return fib_memotd(n,dp);
     }
 }
