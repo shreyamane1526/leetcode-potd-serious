66568834668ecda8f19ab44fdfb11ebe519f0157
shreyamane1526 <shreyamane1526@gmail.com>
2025-11-15 13:04:51 +0530

    Time: 0 ms (100%), Space: 47.6 MB (15.14%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,24 +1,24 @@
 class Solution {
-    public static double powm(double prod,double pow,long n){
-        if(n<=0){return prod;} 
-        if(n%2==1){       
-        prod=prod*pow;
+    public static double pow(double x,int n,double pow,double prod){
+        if(n==0){return prod;}
+        if(n%2==1){
+            prod=prod*pow;
         }
-        pow=pow*pow;
+        pow*=pow;
         n/=2;
-        return powm(prod,pow,n);
+        return pow(x,n,pow,prod);
     }
     public double myPow(double x, int n) {
-        boolean flag=true;
-        long p=n;
-       if(n<0){
-        flag=false;
-        p=Math.abs((long)n);
-       }
-       double prod=1,pow=x;
+        if(x!=1&& x!=-1 && n==Integer.MIN_VALUE){return 0;}
+        if((x==1 || x==-1 )&&  n==Integer.MIN_VALUE){return 1;}
 
-       double ans=powm(prod,pow,p);
-       if(!flag){return 1/ans;}
-       return ans;
+        double prod=1;
+        boolean flag=false;
+        if(n<0){flag=true;}
+        double pow=x;
+        if(flag){
+            return 1/pow(x,-n,pow,prod);
+        }
+        return pow(x,n,pow,prod);
     }
 }
