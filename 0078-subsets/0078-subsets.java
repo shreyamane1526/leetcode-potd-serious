77fe8c6a01615b0ca55f032d808cd3a203059ab6
shreyamane1526 <shreyamane1526@gmail.com>
2026-01-13 18:24:05 +0530

    Time: 0 ms (100%), Space: 44.3 MB (42.22%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,18 +1,16 @@
 class Solution {
-    public static void powset(List<List<Integer>> l2,List<Integer> l1,int i,int nums[]){
-        if(i==nums.length){
-            l2.add(new ArrayList<>(l1));
-            return;
-        }
-        l1.add(nums[i]);
-        powset(l2,l1,i+1,nums);
-        l1.remove(l1.size()-1);
-        powset(l2,l1,i+1,nums);
-    }
     public List<List<Integer>> subsets(int[] nums) {
-       List<List<Integer>> l2=new ArrayList<>();
-       List<Integer> l1=new ArrayList<>();
-       powset(l2,l1,0,nums);
-       return l2;
+       List<List<Integer>> ans=new ArrayList<>();
+       int n=nums.length;
+       for(int i=0;i<(1<<n);i++){
+        List<Integer> temp=new ArrayList<>();
+        for(int j=0;j<n;j++){
+           if((i&(1<<j))>0){ 
+            temp.add(nums[j]);
+           }
+        }
+        ans.add(temp);
+       }
+      return ans;
     }
 }
