fa8fffbaaef1b9ac9958c4c320ab57988c4e9b31
shreyamane1526 <shreyamane1526@gmail.com>
2026-04-20 00:46:22 +0530

    Time: 2 ms (43.09%), Space: 47.6 MB (25.52%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,4 +1,38 @@
+class DSU{
+    int par[],size[];
+    DSU(int V){
+        par=new int[V];
+        size=new int[V];
+        for(int i=0;i<V;i++){
+            par[i]=i;
+            size[i]=1;
+        }
+    }
+    int find(int x){
+        int cur=x;
+        while(par[cur]!=cur){
+            cur=par[cur];
+        }
+        return cur;
+    }
+    void unionBySize(int x,int y){
+        int parX=find(x);
+        int parY=find(y);
+        if(parX==parY){
+            return;
+        }
+        if(size[parX]>=size[parY]){
+            par[parY]=parX;
+            size[parX]+=size[parY];
+        }
+        else{
+            par[parX]=parY;
+            size[parY]+=size[parX];
+        }
+    }
+}
 class Solution {
+    /**********====USING BFS=====******* 
     public void bfs(int isConnected[][],boolean vis[],int j){
         ArrayDeque<Integer> q=new ArrayDeque<>();
         q.offer(j);
@@ -21,6 +55,26 @@ class Solution {
         for(int i=0;i<n;i++){
             if(!vis[i]){
                 bfs(isConnected,vis,i);
+                count++;
+            }
+        }
+        return count;
+    }
+    ********************/
+    //using dsu
+    public int findCircleNum(int[][] isConnected) {
+        int count=0;
+        int n=isConnected.length;
+        DSU dsu=new DSU(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    dsu.unionBySize(i,j);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(dsu.par[i]==i){
                 count++;
             }
         }
