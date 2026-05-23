f60d85902e48c88e45ff88695f7371f5277de810
shreyamane1526 <shreyamane1526@gmail.com>
2026-05-23 18:06:40 +0530

    Time: 6 ms (10.11%), Space: 50.2 MB (54.65%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -29,6 +29,28 @@ class Solution {
         }
         return dp[i][j]=grid[i][j]+Math.min(l,r);
     }
+    int tabu(int grid[][],int m,int n){
+        int dp[][]=new int[m][n];       
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=grid[i][j];
+                }
+                else{
+                    int right=Integer.MAX_VALUE;
+                    int down=Integer.MAX_VALUE;
+                    if(i-1>=0){
+                        right=grid[i][j]+dp[i-1][j];
+                    }
+                    if(j-1>=0){
+                        down=grid[i][j]+dp[i][j-1];
+                    }
+                    dp[i][j]=Math.min(right,down);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
     public int minPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
@@ -36,12 +58,14 @@ class Solution {
         //recur
         return recur(grid,m,n,0,0,0);
         */
-        
+        /*
         //memoisation
         int dp[][]=new int[m][n];
         for(int i=0;i<m;i++){ 
             Arrays.fill(dp[i],Integer.MAX_VALUE);
         }
         return memoi(grid,m,n,0,0,dp);
+        */
+        return tabu(grid,m,n);
     }
 }
