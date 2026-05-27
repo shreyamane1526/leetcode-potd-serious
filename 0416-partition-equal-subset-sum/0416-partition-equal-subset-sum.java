3bfe523df0ae6e9876ea8d6a419e0e8b9b4bad7b
shreyamane1526 <shreyamane1526@gmail.com>
2026-05-27 15:28:32 +0530

    Time: 65 ms (36.03%), Space: 48.8 MB (40.13%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -57,6 +57,29 @@ class Solution {
         }
         return dp[n-1][tar];
     }
+    boolean tabuWithSO(int nums[],int n,int tar){
+        boolean dp[]=new boolean[tar+1];
+        dp[0]=true;
+        if(nums[0]<=tar){ 
+            dp[nums[0]]=true; 
+        }      
+        for(int i=1;i<n;i++){
+            boolean temp[]=new boolean[tar+1];
+            temp[0]=true;
+            for(int j=1;j<=tar;j++){
+                boolean notTake=dp[j];
+                boolean take=false;
+                if(nums[i]<=j){
+                    take=dp[j-nums[i]];
+                }
+                temp[j]=take || notTake;
+            }
+            for(int j=0;j<=tar;j++){
+                dp[j]=temp[j];
+            }
+        }
+        return dp[tar];
+    }
     public boolean canPartition(int[] nums) {
         int tot=0;
         int n=nums.length;
@@ -70,6 +93,7 @@ class Solution {
         //int dp[][]=new int[n][(tot/2)+1];
         //return targetSumMemoi(nums,n-1,tot/2,dp);
         boolean dp[][]=new boolean[n][(tot/2)+1];
-        return tabu(nums,n-1,tot/2,dp);
+        //return tabu(nums,n-1,tot/2,dp);
+        return tabuWithSO(nums,n-1,tot/2);
     }
 }
