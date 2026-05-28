e861bd535391cf0dfea2c2f0066764756feae5ec
shreyamane1526 <shreyamane1526@gmail.com>
2026-05-28 14:33:51 +0530

    Time: 19 ms (37.05%), Space: 47 MB (23.8%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -36,6 +36,34 @@ class Solution {
         }
         return dp[n][amount]=Math.min(take,notTake);
     }
+    int tabu(int coins[],int amount,int n){
+        int dp[][]=new int[n][amount+1];
+        for(int j=0;j<=amount;j++){
+            if(j%coins[0]==0){ 
+                dp[0][j]=j/coins[0];
+            }
+            else{
+                dp[0][j]=Integer.MAX_VALUE;
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int notPick=dp[i-1][j];
+                int pick=Integer.MAX_VALUE;
+                if(coins[i]<=j){
+                    if(dp[i][j-coins[i]]!=Integer.MAX_VALUE){
+                        pick=1+dp[i][j-coins[i]];
+                        
+                    }
+                }
+                dp[i][j]=Math.min(pick,notPick);
+            }
+        }
+        if(dp[n-1][amount]==Integer.MAX_VALUE){
+            return -1;
+        }
+        return dp[n-1][amount];
+    }
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         /*   rceur
@@ -45,6 +73,8 @@ class Solution {
         }
         return ans;     
         */  
+
+        /*
         //memoise
         int dp[][]=new int[n][amount+1];
         for(int i=0;i<n;i++){
@@ -55,5 +85,8 @@ class Solution {
             return -1;
         }
         return ans; 
+        */
+
+        return tabu(coins,amount,n);
     }
 }
