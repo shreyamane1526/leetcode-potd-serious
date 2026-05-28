6ba7f95781141bcf8f05e5e67740313e1ff435ae
shreyamane1526 <shreyamane1526@gmail.com>
2026-05-28 16:09:35 +0530

    Time: 456 ms (31.55%), Space: 43.1 MB (76.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,42 +1,4 @@
 class Solution {
-    /*
-    [1,1,1,1,1], target=3
-    int idx=4,
-    neg=
-        -1,3
-            -2,2
-                -3,1
-                    -4,0
-                        -5,-1
-                        -3,-1
-                    -2,0
-                        -3,-1
-                        -1,-1
-                -1,1
-                    -2,0
-                        -3,-1
-                        -1,-1
-                    0,0
-                        -1,-1
-                         1,-1
-            0,2
-                -1,1
-                    -2,0
-                        -3,-1
-                        -1,-1
-                    0,0
-                        -1,-1
-                         1,-1
-                1,1
-                    0,0
-                        -1,-1
-                        1,-1
-                    2,0
-                        -1,-1
-                         3,-1
-
-    pos=
-    */
     int recur(int[] nums,int target,int n,int cur){
         if(n==-1){
             if(cur==target){ 
@@ -48,8 +10,36 @@ class Solution {
         int pos=recur(nums,target,n-1,cur+nums[n]);
         return neg+pos;
     }
+    
+    int recurTrick(int nums[],int target,int n,int cur,int totSum){
+        if(n==-1){
+            if(2*cur-totSum==target){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        int pick=0;
+        if(2*(cur+nums[n])-totSum<=target){pick=recurTrick(nums,target,n-1,cur+nums[n],totSum);}
+        int notPick=recurTrick(nums,target,n-1,cur,totSum);
+        return pick+notPick;
+    }
+    int memoiseTrick(){
+        return 0;
+    }
+    int tabu(){
+        return 0;
+    }
     public int findTargetSumWays(int[] nums, int target) {
         int n=nums.length;
-        return recur(nums,target,n-1,0);
+        //return recur(nums,target,n-1,0);
+        int totSum=0;
+        for(int i=0;i<n;i++){
+            //2s1-totSum=target
+            //s1+s2=totSum
+            totSum+=nums[i];
+        }
+        return recurTrick(nums,target,n-1,0,totSum);
     }
 }
