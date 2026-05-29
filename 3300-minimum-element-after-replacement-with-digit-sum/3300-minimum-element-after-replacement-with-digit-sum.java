c328ba9f7c8fae6d886745b342bd8434691cb05b
shreyamane1526 <shreyamane1526@gmail.com>
2026-05-29 10:36:14 +0530

    Time: 1 ms (100%), Space: 44.8 MB (55.29%) - LeetHub

---
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    int digitSum(int nums[]){
+        int n=nums.length,sum=0,min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){ 
+                int a=nums[i];
+                int r=0;
+                while(a!=0){
+                    r+=a%10;
+                    a/=10;
+                }
+                nums[i]=r;
+                if(r<min){
+                    min=r;
+                }
+        }
+        return min;
+    }
+    public int minElement(int[] nums) {
+        return digitSum(nums);
+    }
+}
