3d0f7ba1eb7a1ea3d1d30f19de842612a7cbe823
shreyamane1526 <shreyamane1526@gmail.com>
2026-05-30 17:03:19 +0530

    Time: 26 ms (5.69%), Space: 46.4 MB (40.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,12 +1,31 @@
 class Solution {
+    boolean hashing(String s,String t){
+        int n1=s.length();int n2=t.length();
+        if(n1!=n2){
+            return false;
+        }
+        HashMap<Character,Integer> hm1=new HashMap<>();
+        HashMap<Character,Integer> hm2=new HashMap<>();
+        for(int i=0;i<n1;i++){
+            hm1.put(s.charAt(i),hm1.getOrDefault(s.charAt(i),0)+1);
+            hm2.put(t.charAt(i),hm2.getOrDefault(t.charAt(i),0)+1);
+        }    
+        for(Map.Entry<Character,Integer> entry:hm1.entrySet()){
+            int val1=entry.getValue();
+            char key=entry.getKey();
+            if(!hm2.containsKey(key) || hm2.get(key)!=val1){
+                return false;
+            }
+        }
+        return true;
+    }
     public boolean isAnagram(String s, String t) {
+        /*
         int ch[]=new int[26];
         int ch2[]=new int[26];
         s=s.toLowerCase();
         t=t.toLowerCase();
-        if(s.length()!=t.length()){
-            return false;
-        }
+        
         for(int i=0;i<s.length();i++){
             ch[s.charAt(i)-'a']++;
             ch2[t.charAt(i)-'a']++;
@@ -17,5 +36,7 @@ class Solution {
             }
         }
         return true;
+        */
+        return hashing(s,t);
     }
 }
