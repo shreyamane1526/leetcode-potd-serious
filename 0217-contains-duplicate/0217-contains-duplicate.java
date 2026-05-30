a835a603e857719fc7df9f7bf49297405953f490
shreyamane1526 <shreyamane1526@gmail.com>
2026-05-30 16:30:47 +0530

    Time: 15 ms (62.88%), Space: 97 MB (13.92%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean containsDuplicate(int[] arr) {
+    public boolean sorting(int arr[]){
         Arrays.sort(arr);
         for(int i=0;i<arr.length-1;i++){
             if(arr[i]==arr[i+1]){
@@ -7,5 +7,22 @@ class Solution {
             }
         }
         return false;
+    }
+    public boolean hashing(int arr[]){
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(hm.containsKey(arr[i])){
+                return true;
+            }
+            else{
+                hm.put(arr[i],1);
+            }
+        }
+        return false;
+    }
+    public boolean containsDuplicate(int[] arr) {
+        //return sorting(arr);
+        return hashing(arr);
     }
 }
