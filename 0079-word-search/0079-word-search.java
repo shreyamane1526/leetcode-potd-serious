03bb180bb77852e40c3ed99287bb61a69c2af880
Shreya Mane <shreyamane1526@gmail.com>
2026-08-01 13:37:30 +0530

    Time: 154 ms (30.11%), Space: 43 MB (60.85%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,36 +1,37 @@
 class Solution {
     int dx[]={0,0,1,-1};
     int dy[]={1,-1,0,0};
-    boolean dfs(int i,int j,char board[][],String word,int idx,boolean pathVis[][],int n,int m,int len){
+    boolean dfs(int i,int j,char board[][],String word,int idx,int n,int m,int len){
         if(idx+1==len){
             return true;
         }
-        pathVis[i][j]=true;
+        char ch=board[i][j];
+        board[i][j]='.';
         for(int k=0;k<4;k++){
             int newi=i+dx[k],newj=j+dy[k];
-            if(newi>=0 && newj>=0 && newi<n && newj<m && !pathVis[newi][newj] && word.charAt(idx+1)==board[newi][newj]){              
+            if(newi>=0 && newj>=0 && newi<n && newj<m  && word.charAt(idx+1)==board[newi][newj]){              
                 if(idx+1==len-1){
                     return true;
                 }
-                pathVis[newi][newj]=true;
-                if(dfs(newi,newj,board,word,idx+1,pathVis,n,m,len)){
+                char c=board[newi][newj];
+                board[newi][newj]='.';
+                if(dfs(newi,newj,board,word,idx+1,n,m,len)){
                     return true;
                 }
-                pathVis[newi][newj]=false;
+                board[newi][newj]=c;
             }
         }
-        pathVis[i][j]=false;
+        board[i][j]=ch;
         return false;
     }
     public boolean exist(char[][] board, String word) {
         int n=board.length;
         int m=board[0].length;
         int len=word.length();
-        boolean pathVis[][]=new boolean[n][m];
         boolean flag=false;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){               
-                if(board[i][j]==word.charAt(0) && dfs(i,j,board,word,0,pathVis,n,m,len)){
+                if(board[i][j]==word.charAt(0) && dfs(i,j,board,word,0,n,m,len)){
                     return true;
                 }
             }
